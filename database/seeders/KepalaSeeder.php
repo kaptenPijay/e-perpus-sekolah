d0f0e80178a7c82e2e9e5500371040e19e4d45f3
kaptenPijay <muhammadzahran0205@gmail.com>
2024-11-20 16:28:32 +0700

    initial commit

---
+++ database/seeders/KepalaSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class KepalaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'nama' => 'Kepala Sekolah',
+            'email' => 'kepalasekolah@gmail.com',
+            'role' => 'admin',
+            'identitas' => '222222',
+            'alamat' => 'jambi',
+            'password' => Hash::make('123'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
